8229b90e9e1a473207ef7c30899e4b0d04958a7c
Finogeeva Ekaterina <catherine.finogeeva@gmail.com>
2021-03-02 18:26:39 +0400

    finished TodoActivity

--- app/src/main/java/com/example/kotlinwhetherapplication/MainActivity.kt
+++ app/src/main/java/com/example/kotlinwhetherapplication/MainActivity.kt
@@ -1,43 +1,41 @@
 package com.example.kotlinwhetherapplication
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kotlinwhetherapplication.Common.Common
+import com.example.kotlinwhetherapplication.Interface.RetrofitServieces
+import com.example.kotlinwhetherapplication.Model.Forecast
+import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+import java.util.concurrent.Callable
 
 class MainActivity : AppCompatActivity() {
-    private val getdata = GetData()
-    var counter:Int = 0
     val DEGREE = "°"
-    val text:String = "Карл <у Клары> украл <кораллы>, а Клара у Карла"
+    lateinit var mService: RetrofitServieces
+    lateinit var linearLayoutManager: LinearLayoutManager
+    lateinit var alertDialog: AlertDialog
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        general()
-        println(object: Any() {}.javaClass.enclosingMethod?.name)
-        println("1onCreate ${counter++} ${lifecycle.currentState}")
-        lifecycle.addObserver(getdata)
-        if(lifecycle.currentState==Lifecycle.State.CREATED){
-            println("1Created")
-        }
-        if(lifecycle.currentState==Lifecycle.State.DESTROYED){
-            println("1Destroyed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.INITIALIZED){
-            println("1INITIALIZED")
-        }
-        if(lifecycle.currentState==Lifecycle.State.RESUMED){
-            println("1Resumed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.STARTED){
-            println("1Started")
-        }
+        mService = Common.weatherRetrofitServieces
+        linearLayoutManager = LinearLayoutManager(this)
+        alertDialog = SpotsDialog.Builder().setCancelable(true).setContext(this).build()
+
+        getAllWeatherList()
     }
 
 
@@ -49,170 +47,28 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId){
             R.id.todo_id -> intent = Intent(this, TodoActivity::class.java)
+            R.id.marvel_id -> intent = Intent(this, MarvelActivity::class.java)
         }
         startActivity(intent)
         return super.onOptionsItemSelected(item)
     }
 
-    override fun onStart() {
-        super.onStart()
-        println("1onStart ${counter++} ${lifecycle.currentState}")
-        if(lifecycle.currentState==Lifecycle.State.CREATED){
-            println("1Created")
-        }
-        if(lifecycle.currentState==Lifecycle.State.DESTROYED){
-            println("1Destroyed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.INITIALIZED){
-            println("1INITIALIZED")
-        }
-        if(lifecycle.currentState==Lifecycle.State.RESUMED){
-            println("1Resumed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.STARTED){
-            println("1Started")
-        }
-    }
+    fun getAllWeatherList(){
+//        alertDialog.show()
+        mService.getWeatherList().enqueue(object: Callback<Forecast>{
+            override fun onFailure(call: Call<Forecast>, t: Throwable) {
+                call.cancel()
+            }
 
-    override fun onResume() {
-        super.onResume()
-        println("1on Resume ${counter++} ${lifecycle.currentState}")
-        if(lifecycle.currentState==Lifecycle.State.CREATED){
-            println("1Created")
-        }
-        if(lifecycle.currentState==Lifecycle.State.DESTROYED){
-            println("1Destroyed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.INITIALIZED){
-            println("1INITIALIZED")
-        }
-        if(lifecycle.currentState==Lifecycle.State.RESUMED){
-            println("1Resumed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.STARTED){
-            println("1Started")
-        }
-    }
+            override fun onResponse(call: Call<Forecast>, response: Response<Forecast>) {
+                currentTemp.text = response.body()?.main?.temp.toString()
+                lowTemp.text = response.body()?.main?.temp_min.toString()
+                highTemp.text = response.body()?.main?.temp_max.toString()
 
-    override fun onRestart() {
-        super.onRestart()
-        println("1onRestart ${counter++} ${lifecycle.currentState}")
-        if(lifecycle.currentState==Lifecycle.State.CREATED){
-            println("1Created")
-        }
-        if(lifecycle.currentState==Lifecycle.State.DESTROYED){
-            println("1Destroyed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.INITIALIZED){
-            println("1INITIALIZED")
-        }
-        if(lifecycle.currentState==Lifecycle.State.RESUMED){
-            println("1Resumed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.STARTED){
-            println("1Started")
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        println("1onPause ${counter++} ${lifecycle.currentState}")
-        if(lifecycle.currentState==Lifecycle.State.CREATED){
-            println("1Created")
-        }
-        if(lifecycle.currentState==Lifecycle.State.DESTROYED){
-            println("1Destroyed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.INITIALIZED){
-            println("1INITIALIZED")
-        }
-        if(lifecycle.currentState==Lifecycle.State.RESUMED){
-            println("1Resumed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.STARTED){
-            println("1Started")
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        println("1onStop ${counter++} ${lifecycle.currentState}")
-        if(lifecycle.currentState==Lifecycle.State.CREATED){
-            println("1Created")
-        }
-        if(lifecycle.currentState==Lifecycle.State.DESTROYED){
-            println("1Destroyed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.INITIALIZED){
-            println("1INITIALIZED")
-        }
-        if(lifecycle.currentState==Lifecycle.State.RESUMED){
-            println("1Resumed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.STARTED){
-            println("1Started")
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("1onDestroy ${counter++} ${lifecycle.currentState}")
-        if(lifecycle.currentState==Lifecycle.State.CREATED){
-            println("1Created")
-        }
-        if(lifecycle.currentState==Lifecycle.State.DESTROYED){
-            println("1Destroyed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.INITIALIZED){
-            println("1INITIALIZED")
-        }
-        if(lifecycle.currentState==Lifecycle.State.RESUMED){
-            println("1Resumed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.STARTED){
-            println("1Started")
-        }
+//                alertDialog.dismiss()
+            }
+        })
     }
 
 
-
-
-    private fun general(){
-        val subText:String = text.substringAfter('<')
-        firstWord.text = subText.substringBefore('>')
-        secondWord.text = subText.substringAfter('<').substringBefore('>')
-
-        val name = "Kate"
-        val age = 22
-        val greeting = "Hi, there. My name is $name. I'm $age years old"
-        currentTemp.text = greeting
-        val currentTempText: String = currentTemp.text.toString()
-        showGreetings("Hello there")
-        val answer = sum(2, 5)
-        println("answer = $answer")
-
-        val main = Main()
-        main.temp = 90
-        main.minTemp = 20
-        main.maxTemp = 100
-
-        val weather = Weather()
-        weather.main = main
-        presentTemp(main)
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun presentTemp(main: Main) {
-        currentTemp.text = "${main.temp}$DEGREE"
-        lowTemp.text ="${main.minTemp}$DEGREE"
-        highTemp.text = "${main.maxTemp}$DEGREE"
-
-    }
-
-    private fun sum(num1: Int, num2: Int): Int{
-        return num1 + num2
-    }
-    private fun showGreetings(text: String){
-        println(text)
-    }
 }
--- app/src/main/java/com/example/kotlinwhetherapplication/TodoActivity.kt
+++ app/src/main/java/com/example/kotlinwhetherapplication/TodoActivity.kt
@@ -5,116 +5,63 @@ import android.os.Bundle
 import android.util.SparseBooleanArray
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_todo.*
 import java.io.*
+import java.util.ArrayList
 
 
 class TodoActivity : AppCompatActivity() {
-
-    private val todoList = mutableListOf<String>()
-    private val activeInd = mutableListOf<Int>()
+    lateinit var listAdapter: MyArrayAdapter
 //    var imageView = findViewById(R.id.icon) as ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
 
+        listAdapter = MyArrayAdapter(this)
         open()
-
-        var listAdapter = ArrayAdapter<String>(this,
-            android.R.layout.simple_expandable_list_item_2,  todoList)
+        listAdapter.setModeSelected("all")
+        listAdapter.setShowNow()
+        listView.adapter = listAdapter
 
         inputButton.setOnClickListener {
             val inputText: String = inputEditText.text.toString()
             println("input button val $inputText")
             if (inputText != "") {
-                todoList.add(inputText)
-                activeInd.add(todoList.size - 1)
-                println("input button list $todoList")
-                listView.adapter = listAdapter
+                listAdapter.addIntoCheckSelected(false)
+                listAdapter.addIntoListItems(inputText)
+                listAdapter.setShowNow()
+                listAdapter.notifyDataSetChanged()
                 inputEditText.text.clear()
             }
         }
 
-        listView.onItemClickListener = OnItemClickListener { parent, v, position, id ->
-            val selected: SparseBooleanArray = listView.checkedItemPositions
-            activeInd.clear()
-            for (i in todoList.size - 1 downTo 0) {
-                if (!selected[i]){
-                    activeInd.add(i)
-                }
-            }
-        }
-
         allInput.setOnClickListener {
-
-            listAdapter = ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_multiple_choice,  todoList)
-            listView.adapter = listAdapter
-            for(i in 0 until todoList.size) {
-                if (i !in activeInd){
-                    listView.setItemChecked(i, true)
-                }
-            }
+            print("All Button ")
+            println("checkArr - ${MyArrayAdapter.getCheckSelected()}")
+            listAdapter.setModeSelected("all")
+            listAdapter.setShowNow()
+            listAdapter.notifyDataSetChanged()
         }
 
         activeInput.setOnClickListener {
-            val activeList = mutableListOf<String>()
-            activeInd.sort()
-            for(i in activeInd){
-                activeList.add(todoList[i])
-            }
-            listAdapter = ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_multiple_choice,  activeList)
-            listView.adapter = listAdapter
+            print("Active Button ")
+            println("checkArr - ${MyArrayAdapter.getCheckSelected()}")
+            listAdapter.setModeSelected("active")
+            listAdapter.setShowNow()
+            listAdapter.notifyDataSetChanged()
         }
 
         completedInput.setOnClickListener {
-            val completedList = mutableListOf<String>()
-            for (i in 0 until todoList.size){
-                if (i !in activeInd){
-                    completedList.add(todoList[i])
-                }
-            }
-            listAdapter = ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_multiple_choice,  completedList)
-            listView.adapter = listAdapter
-            for(i in 0 until completedList.size){
-                listView.setItemChecked(i, true)
-            }
-        }
-
-        println("2onCreate ${lifecycle.currentState}")
-        if(lifecycle.currentState==Lifecycle.State.CREATED){
-            println("2Created")
-        }
-        if(lifecycle.currentState==Lifecycle.State.DESTROYED){
-            println("2Destroyed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.INITIALIZED){
-            println("2INITIALIZED")
-        }
-        if(lifecycle.currentState==Lifecycle.State.RESUMED){
-            println("2Resumed")
-        }
-        if(lifecycle.currentState==Lifecycle.State.STARTED){
-            println("2Started")
-        }
-    }
-
-    fun printAll(){
-        val listAdapter = ArrayAdapter<String>(this,
-            android.R.layout.simple_list_item_multiple_choice,  todoList)
-        listView.adapter = listAdapter
-        for(i in 0 until todoList.size) {
-            if (i !in activeInd){
-                listView.setItemChecked(i, true)
-            }
+            print("Completed button ")
+            println("checkArr - ${MyArrayAdapter.getCheckSelected()}")
+            listAdapter.setModeSelected("completed")
+            listAdapter.setShowNow()
+            listAdapter.notifyDataSetChanged()
         }
     }
 
@@ -123,27 +70,21 @@ class TodoActivity : AppCompatActivity() {
         if (text != ""){
             val myClass = Gson().fromJson(text, Array<DataItems> :: class.java).toList()
             for (ob in myClass){
-                todoList.add(ob.thing)
-                if(ob.state){
-                    activeInd.add(myClass.indexOf(ob))
-                }
+                listAdapter.addIntoCheckSelected(ob.state)
+                listAdapter.addIntoListItems(ob.thing)
                 println("open: ${ob.thing} - ${ob.state}")
             }
         }
-        printAll()
         Toast.makeText(this, "Данные загружены", Toast.LENGTH_SHORT).show()
     }
 
     fun save() {
         val jsonArr = mutableListOf<DataItems>()
-        println("todoList $todoList")
-        println("activeInd $activeInd")
-//        println("completedInd $completedInd")
-        for (i in 0 until todoList.size){
-            val valString = todoList[i]
-            val state = i in activeInd //if (i+1 in activeArr) true else false
+        println("mList ${listAdapter.getListItems()}")
+        for (i in 0 until listAdapter.getListItems().size){
+            val valString = listAdapter.getListItems()[i]
+            val state = MyArrayAdapter.getCheckSelected(i)//i in completedInd //if (i+1 in activeArr) true else false
             jsonArr.add(DataItems(valString, state))
-//            println("$valString - $state - ${jsonArr[i].thing} ${jsonArr[i].state}")
         }
         val myJSON = Gson().toJson(jsonArr)
         println("______________________________________")
@@ -202,100 +143,4 @@ class TodoActivity : AppCompatActivity() {
         println("todo onStop")
         save()
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    override fun onStart() {
-        super.onStart()
-        println("2onStart ${lifecycle.currentState}")
-        if(lifecycle.currentState== Lifecycle.State.CREATED){
-            println("2Created")
-        }
-        if(lifecycle.currentState== Lifecycle.State.DESTROYED){
-            println("2Destroyed")
-        }
-        if(lifecycle.currentState== Lifecycle.State.INITIALIZED){
-            println("2INITIALIZED")
-        }
-        if(lifecycle.currentState== Lifecycle.State.RESUMED){
-            println("2Resumed")
-        }
-        if(lifecycle.currentState== Lifecycle.State.STARTED){
-            println("2Started")
-        }
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        println("2onRestart ${lifecycle.currentState}")
-        if(lifecycle.currentState== Lifecycle.State.CREATED){
-            println("2Created")
-        }
-        if(lifecycle.currentState== Lifecycle.State.DESTROYED){
-            println("2Destroyed")
-        }
-        if(lifecycle.currentState== Lifecycle.State.INITIALIZED){
-            println("2INITIALIZED")
-        }
-        if(lifecycle.currentState== Lifecycle.State.RESUMED){
-            println("2Resumed")
-        }
-        if(lifecycle.currentState== Lifecycle.State.STARTED){
-            println("2Started")
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        println("2onPause ${lifecycle.currentState}")
-        if(lifecycle.currentState== Lifecycle.State.CREATED){
-            println("2Created")
-        }
-        if(lifecycle.currentState== Lifecycle.State.DESTROYED){
-            println("2Destroyed")
-        }
-        if(lifecycle.currentState== Lifecycle.State.INITIALIZED){
-            println("2INITIALIZED")
-        }
-        if(lifecycle.currentState== Lifecycle.State.RESUMED){
-            println("2Resumed")
-        }
-        if(lifecycle.currentState== Lifecycle.State.STARTED){
-            println("2Started")
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("2onDestroy ${lifecycle.currentState}")
-        if(lifecycle.currentState== Lifecycle.State.CREATED){
-            println("2Created")
-        }
-        if(lifecycle.currentState== Lifecycle.State.DESTROYED){
-            println("2Destroyed")
-        }
-        if(lifecycle.currentState== Lifecycle.State.INITIALIZED){
-            println("2INITIALIZED")
-        }
-        if(lifecycle.currentState== Lifecycle.State.RESUMED){
-            println("2Resumed")
-        }
-        if(lifecycle.currentState== Lifecycle.State.STARTED){
-            println("2Started")
-        }
-    }
-
-
 }
